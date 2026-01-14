41b2398e467f9ac23047dfb1026463cc21590ab0
Shahbaz Khan <shahbazkhan19111@gmail.com>
2026-01-14 16:54:23 +0000

    remove logo icon

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
-import { BookMarked } from 'lucide-react';
 
 const Logo = ({ className }: { className?: string }) => {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-2xl font-bold tracking-tighter font-logo text-foreground", className)}>
-      <BookMarked className="h-6 w-6 text-primary" />
       <span className='whitespace-nowrap'>Falcon News</span>
     </Link>
   );
