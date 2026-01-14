39a62dfd56e3430cff013ccddbc451325a7751e8
Shahbaz Khan <shahbazkhan19111@gmail.com>
2026-01-14 16:50:45 +0000

    change the editorial to Falcon News  in bold

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = ({ className }: { className?: string }) => {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-2xl font-bold tracking-tighter font-logo text-foreground", className)}>
       <BookMarked className="h-6 w-6 text-primary" />
-      <span className='whitespace-nowrap'>The Editorial</span>
+      <span className='whitespace-nowrap'>Falcon News</span>
     </Link>
   );
 };
